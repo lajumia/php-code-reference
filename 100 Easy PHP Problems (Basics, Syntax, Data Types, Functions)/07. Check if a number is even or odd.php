7c5995f83fbb0aca6e5608c883e7d1fa8e43f37a
Md Laju Mia <developerlaju@gmail.com>
2025-04-14 06:32:14 +0600

    Find the maximum of two numbers

---
+++ 100 Easy PHP Problems (Basics, Syntax, Data Types, Functions)/07. Check if a number is even or odd.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$a = 34;
+$b = 234;
+
+function find_max_number($number1, $number2){
+
+    if($number1 == $number2){
+    echo "The number is equal ";
+
+    }elseif($number1 > $number2){
+        echo "The larger Number is ". $number1;
+    }else{
+        echo "The larger Number is ". $number2;
+    }
+
+}
+
+find_max_number($a,$b);// The larger Number is 234
+
+?>
+
