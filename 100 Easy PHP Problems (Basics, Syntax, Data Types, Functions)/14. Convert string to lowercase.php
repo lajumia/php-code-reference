a57e05fe7a8e767cf131668cbdac2b568b21effd
Md Laju Mia <developerlaju@gmail.com>
2025-04-14 07:28:15 +0600

    Convert string to lowercase

---
+++ 100 Easy PHP Problems (Basics, Syntax, Data Types, Functions)/14. Convert string to lowercase.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$string = "The NAME OF OUR COUNTRY IS BANGLADESH";
+
+function string_to_lower($string){
+    $string = strtolower($string);
+    echo $string;
+
+}
+
+string_to_lower($string);
