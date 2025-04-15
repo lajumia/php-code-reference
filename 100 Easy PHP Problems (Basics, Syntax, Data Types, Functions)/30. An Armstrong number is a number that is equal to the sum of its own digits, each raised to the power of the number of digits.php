43ba0d54eb9f9cc48cfff24db59e7a1751e94066
Md Laju Mia <developerlaju@gmail.com>
2025-04-15 08:50:31 +0600

    Check Armstrong number

---
+++ 100 Easy PHP Problems (Basics, Syntax, Data Types, Functions)/30. An Armstrong number is a number that is equal to the sum of its own digits, each raised to the power of the number of digits.php
@@ -0,0 +1,29 @@
+<?php
+// An Armstrong number is a number that is equal to the sum of its own digits,
+// each raised to the power of the number of digits.
+//Example--------------------------------------------------
+// 5¹ = 5 → Armstrong 
+// 1³ + 5³ + 3³ = 1 + 125 + 27 = 153 
+// 9⁴ + 4⁴ + 7⁴ + 4⁴ = 6561 + 256 + 2401 + 256 = 9474 
+
+
+$number = 153;
+
+function check_armstrong(){
+    $number_count = strlen($number);
+    $sum = 0;
+
+    for($i=0;$i<=$number_count-1;$i++){
+        $sum += paw($number[$i],$number_count)
+    }
+
+    if($sum == $number){
+        echo "Armstrong";
+    }else{
+        echo "Not Armstrong";
+    }
+}
+
+
+
+check_armstrong($number);
